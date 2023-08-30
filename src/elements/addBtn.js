d090b555b840f95b8977a63e57af07dc5efd81fb
Tommy Chen <tommyjchen@hotmail.com>
2023-08-30 16:26:27 -0400

    responsive design for portrait and landscape

---
+++ src/elements/addBtn.js
@@ -0,0 +1,7 @@
+import addBtnCSS from './addBtn.css';
+
+const addBtn = document.createElement('button');
+addBtn.setAttribute('id', 'add-btn');
+addBtn.innerHTML = '+';
+
+export default addBtn;
--- src/elements/container.js
+++ src/elements/container.js
@@ -3,12 +3,21 @@ import header from './header';
 import nav from './navBar';
 import content from './content';
 import footer from './footer';
+import addBtn from './addBtn';
 const container = document.createElement('div');
 container.setAttribute('id', 'container');
 
+const mid = document.createElement('section');
+mid.setAttribute('id', 'mid');
+mid.appendChild(nav);
+mid.appendChild(content);
+
+
 container.appendChild(header);
-container.appendChild(nav);
-container.appendChild(content);
+// container.appendChild(nav);
+// container.appendChild(content);
+container.appendChild(mid);
 container.appendChild(footer);
+container.appendChild(addBtn);
 
 export default container;
