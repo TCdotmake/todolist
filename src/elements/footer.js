30dcf3afd3bc6b6bb36540b9670bbe3270f8851d
Tommy Chen <tommyjchen@hotmail.com>
2023-09-16 16:04:09 -0400

    remove item is working

--- src/elements/footer.js
+++ src/elements/footer.js
@@ -25,53 +25,6 @@ for(let icon of icons){
     actionBar.appendChild(item);
 }
 
-// // priority icons
-// let lpIcon, mpIcon, hpIcon;
-// const priorityArr = ["Low", "Med", "High"];
-// const btnArr = [];
-// for (let n of priorityArr) {
-//   const btn = mkPriorityIcon(n);
-//   btnArr.push(btn);
-// }
-// [lpIcon, mpIcon, hpIcon] = [...btnArr];
-
-// lpIcon.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     lpIcon.replaceWith(mpIcon);
-//   });
-//   mpIcon.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     mpIcon.replaceWith(hpIcon);
-//   });
-//   hpIcon.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     hpIcon.replaceWith(lpIcon);
-//   });
-
-// // helper functions
-
-// function loadTodo(){
-//     const name = document.getElementById('edit-todo-name');
-//     const desc = document.getElementById('edit-todo-desc');
-//     const due = document.getElementById('edit-due-date');
-//     const icon = document.getElementById('edit-todo-misc').firstChild;
-//     const selected = document.querySelector('.selected');
-//     const id = selected.dataset.id;
-//     const todo = app.getItem(id);
-//     name.value = todo.name;
-//     desc.value = todo.desc;
-//     console.log(todo.due);
-//     due.value = todo.due;
-//     const priority = todo.priority;
-//     if(priority == 'Low'){
-//         icon.replaceWith(lpIcon);
-//     }else if(priority == 'Med'){
-//         icon.replaceWith(mpIcon);
-//     }else{
-//         icon.replaceWith(hpIcon);
-//     }
-// }
-
 //eventlisteners
 editIcon.addEventListener('click',(e)=>{
     e.preventDefault();
@@ -92,6 +45,16 @@ editIcon.addEventListener('click',(e)=>{
         loadTodo();
         document.querySelector(".modal-bg").classList.add("modal-open");
         document.getElementById('edit-todo-modal').classList.add('modal-open');
+    }
+});
+
+trashIcon.addEventListener('click', e=>{
+    e.preventDefault();
+    const selected = document.querySelector('.selected');
+    const type = selected.dataset.type;
+    const id = selected.dataset.id;
+    if(type == 'todo'){
+        app.removeItem(id);
     }
 })
 
