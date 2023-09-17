2bcd7c4584f9a0e9fbe5dd2cb48cabd1906ae350
Tommy Chen <tommyjchen@hotmail.com>
2023-09-17 15:37:20 -0400

    moved filter logic inside app

--- src/elements/updateList.js
+++ src/elements/updateList.js
@@ -3,47 +3,54 @@ import mkList from "./mkList";
 import mkTodoDisplay from "./mkTodoDisplay";
 import filters from "../filters";
 
-const updateList = ()=>{
+const updateList = () => {
+//   const lists = structuredClone(app.children);
+//   const allTodo = [];
+//   for (let list of lists) {
+//     for (let todo of list.children) {
+//       allTodo.push(todo);
+//     }
+//   }
 
-    const lists = structuredClone(app.children);
-    const allTodo = [];
-    for(let list of lists){
-        for(let todo of list.children){
-            allTodo.push(todo);
-        }
-    }
-    console.log('all todo');
-    console.log(allTodo);
-    let filteredTodo = [];
+//   let filteredTodo = [];
+//   const filter = app.filter;
+//   switch (filter) {
+//     case "today":
+//       filteredTodo = allTodo.filter(filters.today);
+//       break;
+//     case "upcoming":
+//       filteredTodo = allTodo.filter(filters.upcoming);
+//       break;
+//     case "anytime":
+//       filteredTodo = allTodo.filter(filters.anytime);
+//       break;
+//     default:
+//       filteredTodo = null;
+//   }
+
+//   console.log(filteredTodo);
+
     const filter = app.filter;
-    if(filter == 'today'){
-        filteredTodo = allTodo.filter(filters.today);
-        console.log('today filter');
+    const filteredItems = [];
+    for(let id of app.filteredItems){
+        filteredItems.push(app.getItem(id));
     }
-    if(filter == 'upcoming'){
-        filteredTodo = allTodo.filter(filters.upcoming);
-        console.log('upcoming filter');
-    }
-    if(filter == 'anytime'){
-        filteredTodo = allTodo.filter(filters.anytime);
-        console.log('anytime filter');
-    }
-    console.log(filteredTodo);
+    console.log(filteredItems);
 
-    const content = document.getElementById('content');
-    //clear out current items if any
-    while(content.firstElementChild){
-        content.firstElementChild.remove();
+  const content = document.getElementById("content");
+  //clear out current items if any
+  while (content.firstElementChild) {
+    content.firstElementChild.remove();
+  }
+  //populate content
+  for (let listObj of app.children) {
+    const item = mkList(listObj);
+    content.appendChild(item);
+    if (listObj.children.length > 0 && listObj.expand) {
+      const todos = mkTodoDisplay(listObj.id);
+      content.appendChild(todos);
     }
-    //populate content
-    for(let listObj of app.children){
-        const item = mkList(listObj);
-        content.appendChild(item);
-    if(listObj.children.length > 0 && listObj.expand){
-        const todos = mkTodoDisplay(listObj.id);
-        content.appendChild(todos);
-    }
-}
-}
+  }
+};
 
 export default updateList;
--- src/managementMethods.js
+++ src/managementMethods.js
@@ -4,14 +4,16 @@ import STORAGE from "./storageName";
 import localLoad from "./localLoad";
 import localSave from "./localSave";
 import updateVisual from "./elements/updateVisual";
+import getToday from "./getToday";
 const managementMethods = {
-
-  updateMemory: function(){
+  updateMemory: function () {
     localSave(STORAGE, this);
     updateVisual();
-    console.log('Store Updated');
+    console.log("Store Updated");
     console.log(localLoad(STORAGE));
   },
+
+  // add, remove, delete
   getID: function () {
     return this.id;
   },
@@ -35,49 +37,124 @@ const managementMethods = {
     parent.children.push(item);
     this.updateMemory();
   },
-  getItem: function (id){
+  getItem: function (id) {
     return this.searchObj(this, id);
   },
-  removeItem: function(id){
+  removeItem: function (id) {
     const parentID = this.getItem(id).parentID;
     const parent = this.getItem(parentID);
     let index = -1;
-    for(let n in parent.children){
-        if(parent.children[n].id == id){
-            index = n;
-        }
+    for (let n in parent.children) {
+      if (parent.children[n].id == id) {
+        index = n;
+      }
     }
     parent.children.splice(index, 1);
     this.updateMemory();
   },
-  addList: function(name){
+  addList: function (name) {
     this.addItem(this.id, this.mkList(name));
   },
-  addTodo: function(parentID, name, desc, due, priority){
+  addTodo: function (parentID, name, desc, due, priority) {
     this.addItem(parentID, this.mkTodo(name, desc, due, priority));
   },
-  editList: function(id, name){
+
+  // edit
+
+  editList: function (id, name) {
     this.getItem(id).name = name;
     this.updateMemory();
   },
-  editTodo: function(id, {name, desc, due, priority}){
+  editTodo: function (id, { name, desc, due, priority }) {
     this.getItem(id).name = name;
     this.getItem(id).desc = desc;
     this.getItem(id).due = due;
     this.getItem(id).priority = priority;
     this.updateMemory();
   },
-  toggleComplete(id){
+  toggleComplete(id) {
     this.getItem(id).complete = !this.getItem(id).complete;
     this.updateMemory();
   },
-  toggleExpand(id){
+  toggleExpand(id) {
     this.getItem(id).expand = !this.getItem(id).expand;
     this.updateMemory();
   },
-  changeFilter(filter){
+
+  // filter
+  changeFilter(filter) {
     this.filter = filter;
+    this.updateFilterItems();
     this.updateMemory();
+  },
+  getToday() {
+    const date = new Date();
+    const year = date.getFullYear();
+    let month = date.getMonth();
+    month += 1;
+    month = month.toString();
+    if (month.length < 2) {
+      month = `0${month}`;
+    }
+    const day = date.getDate();
+    const today = `${year}-${month}-${day}`;
+    return today;
+  },
+  filters: {
+    today(obj) {
+      const today = getToday().toString();
+      const due = obj.due.toString();
+      if (due == today) {
+        return true;
+      }
+      return false;
+    },
+    upcoming(obj) {
+      const today = getToday().toString();
+      const due = obj.due.toString();
+      if (due > today) {
+        return true;
+      }
+      return false;
+    },
+    anytime(obj) {
+      const due = obj.due.toString();
+      if (due == "" || due == null) {
+        return true;
+      }
+      return false;
+    },
+  },
+  getAllTodo() {
+    const allTodo = [];
+    for (let list of this.children) {
+      for (let todo of list.children) {
+        allTodo.push(todo);
+      }
+    }
+    return allTodo;
+  },
+  updateFilterItems() {
+    const filter = this.filter;
+    const allTodo = this.getAllTodo();
+    let filteredTodo = [];
+    switch (filter) {
+      case "today":
+        filteredTodo = allTodo.filter(this.filters.today);
+        break;
+      case "upcoming":
+        filteredTodo = allTodo.filter(this.filters.upcoming);
+        break;
+      case "anytime":
+        filteredTodo = allTodo.filter(this.filters.anytime);
+        break;
+      default:
+        filteredTodo = null;
+    };
+    if(filteredTodo != null){
+      this.filteredItems = filteredTodo.map(todo=>todo.id);
+    }
+    else{this.filteredItems = null;}
   },
 };
 
